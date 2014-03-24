096ad2c6cbaebba0db195082123abd70a011fe5e
aman-sood <dmi-v@DMI-Vs-MacBook-Pro.local>
2014-03-24 18:02:16 +0530

    Factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+import math
+
+number = int(input('Enter number :'))
+print('Factorial is :',math.factorial(number))
+
+#	Without using python function
+#	factorial = 1
+#	while number - 1 > 0:
+#		factorial = number * factorial
+#		number = number - 1
+#	print('Factorial is :',factorial)
+
