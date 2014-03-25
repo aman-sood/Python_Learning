b9574b79071a2d52f4490fbfb75bd320dbe414ed
aman-sood <dmi-v@DMI-Vs-MacBook-Pro.local>
2014-03-25 13:01:12 +0530

    variable function parameters

---
+++ variabile_parameters.py
@@ -0,0 +1,13 @@
+# Taken from Byte of Python
+
+def total(initial=5, *numbers, **keywords):
+	count = initial
+	for number in numbers: 
+		count += number
+	for key in keywords:
+		count += keywords[key]
+	return count
+
+
+# initial = 10, *numbers = 1, 2, 3 and **keywords = 50 , 100
+print(total(10, 1, 2, 3, vegetables=50, fruits=100))
